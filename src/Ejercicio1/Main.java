bfb5ee730d46d1a04047c3272bdb189a91f4fbf4
Eduardo Peña Acosta <usr1565@salesianos-lacuesta.net>
2021-10-29 10:13:15 +0100

    Finalizado ejercicio11

--- src/Ejercicio1/Main.java
+++ src/Ejercicio1/Main.java
@@ -1,9 +1,7 @@
 package Ejercicio1;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import static java.lang.Integer.parseInt;
@@ -13,19 +11,16 @@ public class Main {
     public static void main(String[] args) {
 
 
-        ProcessBuilder ProcessBuilder = new ProcessBuilder();
-        Process process = null;
+        ProcessBuilder processBuilder = new ProcessBuilder();
         int OpenTimesExecutable = 0;
         String navigator = "";
-
-        System.out.println("Start the process...");
 
         if (args.length == 0) {
 
             /*
-            * No hemos detectado argumentos junto a la ejecución del comando, así que
-            * mando una notificación, se abren scanners, se piden datos y se almacena el comando.
-            */
+             * No hemos detectado argumentos junto a la ejecución del comando, así que
+             * mando una notificación, se abren scanners, se piden datos y se almacena el comando.
+             */
 
             System.out.println("No args introduced, please type in in the console the times you want to open the app.");
             Scanner inputScanner = new Scanner(System.in);
@@ -38,51 +33,62 @@ public class Main {
             System.out.print("Choose web browser: ");
             navigator = inputScanner.nextLine();
 
-            ProcessBuilder.command("open","-n","-a",navigator);
+            processBuilder.command("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome");
 
             inputScanner.close();
 
-        }else{
+        } else {
 
             /*
-            * En este ha detectado los argumentos, pasaremos los argumentos a las variables que
-            * hemos creado anterior mente y almacenamos el comando.
-            */
+             * En este ha detectado los argumentos, pasaremos los argumentos a las variables que
+             * hemos creado anterior mente y almacenamos el comando.
+             */
 
             System.out.println("Args introduced");
-            System.out.println("Args 0 -> "+ args[0]);
-            System.out.println("Args 1 -> "+ args[1]);
+            System.out.println("Args 0 -> " + args[0]);
+            System.out.println("Args 1 -> " + args[1]);
 
             OpenTimesExecutable = parseInt(args[0]);
             navigator = args[1];
 
-            ProcessBuilder.command("open","-n","-a",navigator);
+            //processBuilder.command("open", "-n", "-a", navigator);
+
+            processBuilder.command("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome");
+
+
         }
 
-        try{
+        try {
 
             /*
-            * Se ha creado un bucle que abrirá cuantas veces se haya pedido
-            * que se abra el programa, soltará también el código de error en caso de que algo pase
-            */
+             * Se ha creado un bucle que abrirá cuantas veces se haya pedido, se almacenará el proceso mediante un
+             * ArrayList, se arrancará las veces indicadas, donde almacenaremos a la vez el proceso en ArrayList
+             * comprobará si está arrancado y cuando termine
+             */
 
+            ArrayList<Process> processList = new ArrayList<Process>();
+            System.out.println("The process is starting...");
+
+            for (int i = 0; i < OpenTimesExecutable; i++) {
+                Process process = processBuilder.start();
+                processList.add(process);
+
+            }
+
+            //System.out.println("El número de procesos en ejecución es: " + processList.size());
 
             System.out.println("The process is working");
-            for(int i = 0; i < OpenTimesExecutable; i++){
-                process = ProcessBuilder.start();
-                while(process.isAlive()) {
-                    process.waitFor();
-
+            for(Process process : processList){
+                while(process.isAlive()){
                 }
             }
 
             System.out.println("The process has finished");
 
-        }catch(Error | IOException | InterruptedException e){
+    } catch (Error | IOException e) {
             e.printStackTrace();
-            System.out.println("Le falta algún argumento o no ha introducido alguno de los datos correctamente.");
+            System.out.println("Somehow is.");
         }
-
     }
 
 }
--- src/Ejercicio2/Main.java
+++ src/Ejercicio2/Main.java
@@ -1,9 +1,87 @@
 package Ejercicio2;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.Integer.parseInt;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        ProcessBuilder ProcessBuilder = new ProcessBuilder();
+        int OpenTimesExecutable = 0;
+        String navigator = "";
+
+        System.out.println("Start the process...");
+
+        if (args.length == 0) {
+
+            /*
+             * No hemos detectado argumentos junto a la ejecución del comando, así que
+             * mando una notificación, se abren scanners, se piden datos y se almacena el comando.
+             */
+
+            System.out.println("No args introduced, please type in in the console the times you want to open the app.");
+            Scanner inputScanner = new Scanner(System.in);
+
+            System.out.print("Choose the times you want to open the App: ");
+            OpenTimesExecutable = inputScanner.nextInt();
+            //Volvemos a hacer el inputScanner para limpiar la línea.
+            inputScanner.nextLine();
+
+            System.out.print("Choose web browser: ");
+            navigator = inputScanner.nextLine();
+
+            ProcessBuilder.command("open", "-n", "-a", navigator);
+
+            inputScanner.close();
+
+        } else {
+
+            /*
+             * En este ha detectado los argumentos, pasaremos los argumentos a las variables que
+             * hemos creado anterior mente y almacenamos el comando.
+             */
+
+            System.out.println("Args introduced");
+            System.out.println("Args 0 -> " + args[0]);
+            System.out.println("Args 1 -> " + args[1]);
+
+            OpenTimesExecutable = parseInt(args[0]);
+            navigator = args[1];
+
+            ProcessBuilder.command("open", "-n", "-a", navigator);
+        }
+
+        try {
+            ArrayList<Process> processList = new ArrayList<>();
+            /*
+             * Se ha creado un bucle que abrirá cuantas veces se haya pedido
+             * que se abra el programa, soltará también el código de error en caso de que algo pase
+             * Y dentro del bucle se ha agregado un nuevo bucle donde esperaremos que el proceso se termine.
+             */
+
+            System.out.println("The process is working");
+            for (int i = 0; i < OpenTimesExecutable; i++) {
+                processList.add(ProcessBuilder.start());
+            }
+
+            TimeUnit.MILLISECONDS.sleep(500);
+            for( Process process: processList){
+                while(process.isAlive()){
+                    process.destroy();
+                    process.waitFor();
+                }
+            }
+            System.out.println("The process has finished");
+
+        } catch (Error | IOException | InterruptedException e) {
+            e.printStackTrace();
+            System.out.println("Le falta algún argumento o no ha introducido alguno de los datos correctamente.");
+        }
     }
 
 }
